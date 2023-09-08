44b698559745a2f314f1504b4073fe47e89ddad8
Alan Cechi <alancechi188@gmail.com>
2023-09-08 14:46:24 -0300

    exercício 3

--- trabalho.sql
+++ trabalho.sql
@@ -102,3 +102,10 @@ select titulo from livros;
 select NOME from autores
 WHERE nascimento < '1900-01-01';
 
+-- exercício 3
+SELECT titulo 
+from livros 
+inner join autores on autor_id=autores.id
+where NOME = 'J.K. Rowling';
+
+
