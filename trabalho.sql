cd435df2c4d9c86356f7fdd6ad47ef452a3b9e58
Alan Cechi <alancechi188@gmail.com>
2023-09-08 14:52:35 -0300

    exercício 13

--- trabalho.sql
+++ trabalho.sql
@@ -158,6 +158,13 @@ SELECT alunos.nome, matriculas.curso
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id;
 
+-- exercício 13
+SELECT autores.nome, livros.titulo
+FROM autores
+LEFT JOIN livros ON autores.id = livros.autor_id;
+
+
+
 
 
 
